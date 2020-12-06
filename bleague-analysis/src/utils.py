e44f01f8a866859ffa14bf7fb4303c33276daaa3
tishikawa <takayuki.ishikawa@gunosy.com>
2020-12-06 09:12:51 +0900

    Fix

--- bleague-analysis/src/utils.py
+++ bleague-analysis/src/utils.py
@@ -3,7 +3,7 @@ def calc_posession(FGA: int, TO: int, FTA: int, OR: int) -> float:
     ref. https://www.nbastuffer.com/analytics101/possession/
     - FGA: Field Goal Attempts
     - TO: Turnovers
-    - fta: Free Throw Attempts
+    - FTA: Free Throw Attempts
     - OR: Offensive Rebounds
     """
     return 0.96 * (FGA + TO + 0.44 * FTA - OR)
